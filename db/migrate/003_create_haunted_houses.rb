ffa633d8118e86fbd201ecf42903f3dd880e2b6f
Katie Weber <k.weber0893@gmail.com>
2018-06-18 09:44:20 -0400

    Created multiple tables

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |t|
+      t.text :name
+      t.text :location
+      t.text :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.text :description
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.timestamps
+    end
+  end
+end
